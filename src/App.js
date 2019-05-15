7ec4c26cbf6b54f1914969c8ef69450f9d4b85a6
Artem Mironov <amironov@scnsoft.com>
2019-05-15 20:09:21 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="todo"></div>
+    <div className="todo">
+      
+    </div>
   );
 }
 
