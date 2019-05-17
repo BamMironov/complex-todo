1ac282e9a820ceeee3efa9c96ed1bc0fb3ad76e6
Artem Mironov <amironov@scnsoft.com>
2019-05-17 10:52:11 +0300

    Created Fallback for ListScreen

--- src/screens/ListScreen/ListScreen.jsx
+++ src/screens/ListScreen/ListScreen.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Container, Row, Col, ListGroup } from 'reactstrap';
 import { Item } from '../../components';
 import { removeTask, TodoContext } from '../../store';
+import { NavLink } from 'react-router-dom';
 import './ListScreen.scss';
 
 export function ListScreen() {
@@ -9,15 +10,30 @@ export function ListScreen() {
     const onRemoveTask = id => dispatch(removeTask(id));
 
     function TasksList() {
-        return state.tasks.map(item => {
-            return (
-                <Item data={item} key={item.id} onRemove={() => onRemoveTask(item.id)} />
-            )
-        })
+        return (
+            <ListGroup>
+                {
+                    state.tasks.map(item => {
+                        return (
+                            <Item data={item} key={item.id} onRemove={() => onRemoveTask(item.id)} />
+                        )
+                    })
+                }
+            </ListGroup>
+        )
     }
 
     function MainTitle() {
         return <h1>Task List</h1>;
+    }
+
+    function Fallback() {
+        return (
+            <div className='ct-fallback'>
+                <p>It seems like that the list is Empty.</p>
+                <p>Let's <NavLink to='/create'>create</NavLink> a new one!</p>
+            </div>
+        )
     }
 
     return (
@@ -25,9 +41,9 @@ export function ListScreen() {
             <Row className='justify-content-center'>
                 <Col md='6'>  
                     <MainTitle />         
-                    <ListGroup>
-                        <TasksList />
-                    </ListGroup>
+                    {
+                        state.tasks.length ? <TasksList /> : <Fallback />
+                    }
                 </Col>
             </Row>
         </Container>
