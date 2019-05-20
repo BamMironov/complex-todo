cc303d507610039aba64ab4307fe8a8eec1ef871
Artem Mironov <amironov@scnsoft.com>
2019-05-20 11:29:16 +0300

    Validation fix

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -5,27 +5,36 @@ export function useForm(callback, validate, initialValues) {
     const [errors, setErrors] = useState({});
     const [isSubmitting, setIsSubmitting] = useState(false);
 
+    const handleErrors = () => setErrors(validate(values));
+
     const handleSublmit = event => {
         if (event) {
             event.preventDefault();
         }
 
-        setIsSubmitting(true);
-        setErrors(validate(values));
+        handleErrors();
+
+        if (!Object.keys(validate(values)).length) {
+            callback();
+        } else {
+            setIsSubmitting(true);
+        }
     }
 
     const handleChange = event => {
         event.persist();
-        setValues(values => ({ ...values, [event.target.name]: event.target.value }));
-        setErrors(validate(values));
+
+        setValues(values => ({
+            ...values,
+            [event.target.name]: event.target.value
+        }));
     }
 
     useEffect(() => {
-        if (isSubmitting && !Object.keys(errors).length) {
-            callback();
+        if (isSubmitting) {
+            handleErrors();
         }
-        setIsSubmitting(false);
-    }, [errors]);
+    }, [values]);
 
     return {
         values,
